3efa355973a66ad85fad8519ed3784d0f165744b
Micha Helder <micha.helder@hotmail.com>
2023-08-01 17:24:50 +0200

    Fix the Python script

--- PlotReligiousMakeUpChange.py
+++ PlotReligiousMakeUpChange.py
@@ -22,11 +22,10 @@ if __name__ == '__main__':
         t = f.read()
         
         hremembers = get_data(t, 'Amount of HRE Members')
-        hredev = get_data(t, 'Total HRE Development')
         nonabrahamic = get_data(t, 'Nations that are not Abrahamic share of HRE Dev')
-        catholicshare = get_data(t, 'Catholic States Share of HRE Dev')
-        sunnishare = get_data(t, 'Sunni States Share of HRE Dev')
-        orthodoxshare = get_data(t, 'Orthodox States Share of HRE Dev')
+        catholicshare = get_data(t, 'Catholic (ABR) States Share of HRE Dev')
+        sunnishare = get_data(t, 'Sunni (ABR) States Share of HRE Dev')
+        orthodoxshare = get_data(t, 'Orthodox (ABR) States Share of HRE Dev')
         itr = range(1, len(hremembers) + 1)
 
         plt.plot(itr, hremembers, label="Amount of HRE Members")
