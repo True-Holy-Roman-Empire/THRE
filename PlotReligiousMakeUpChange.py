6f4ebf980c3367c1e046162d8dcf1556f6f5bb9d
Micha Helder <micha.helder@hotmail.com>
2023-08-01 16:54:12 +0200

    Add nonabrahamic to Python script for monitoring

--- PlotReligiousMakeUpChange.py
+++ PlotReligiousMakeUpChange.py
@@ -23,13 +23,14 @@ if __name__ == '__main__':
         
         hremembers = get_data(t, 'Amount of HRE Members')
         hredev = get_data(t, 'Total HRE Development')
+        nonabrahamic = get_data(t, 'Nations that are not Abrahamic share of HRE Dev')
         catholicshare = get_data(t, 'Catholic States Share of HRE Dev')
         sunnishare = get_data(t, 'Sunni States Share of HRE Dev')
         orthodoxshare = get_data(t, 'Orthodox States Share of HRE Dev')
         itr = range(1, len(hremembers) + 1)
 
         plt.plot(itr, hremembers, label="Amount of HRE Members")
-        #plt.plot(itr, hredev, label="Total HRE Development")
+        plt.plot(itr, nonabrahamic, label="Nations that are not Abrahamic share of HRE Dev in %")
         plt.plot(itr, catholicshare, label="Catholic States Share of HRE Dev in %")
         plt.plot(itr, sunnishare, label="Sunni States Share of HRE Dev in %")
         plt.plot(itr, orthodoxshare, label="Orthodox States Share of HRE Dev in %")
